4e8be401cd85d27f6c129640e7b6b51d4f30dcab
Weiqiiing <38157148+Weiqiiing@users.noreply.github.com>
2018-08-30 17:25:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from discord.ext.commands import Bot
 xpban=[[""] * 2 for i in range(1)]
 
 sectList = ["Autarch Flipping", "Explosion","Blank","Thousand and One Petals","Debauchery Tea Party","Sort Post A Massege", "Rising Peak","NOT A GAE"] #Name of Sect
-sectCall = ["ELON","Exp","Blank","PETALS","TeaParty","spam","Lord","gay"] #call for .!sect <name>
+sectCall = ["ELON","Exp","Blank","PETALS","TeaParty","spam","Lord","gae"] #call for .!sect <name>
 sectTags = ["「 ELON 」","Explosion","『　　』","《 PETALS 》","[TeaParty]","[spam]","Lord","【﻿GAE】"] #Tag required for xp   
 sectOwner = ["Perpetual Phoenix", "Megumin_Explosion", "Storm","Ziyun","ZaChan","VANSTORM","Modsan","WitheringLeaf"] #Owner of Sect
 sectDescription = ["Listen, ye mortal of the finite realms, for We are the Autarchs that were present at the beginning, and shall be present at the end! We are the Holy Inheritors of Elon, transcendent of all other races! We have seen generations trickle by, in the river of history, yet remain unfettered by time! Those who proclaim themselves as people of power are nothing to Us, for there is no apex past Us! A force that rivals the Heavens! A force that has become the Heavens! Who dares claim to be our equal‽ ",  #Elon
