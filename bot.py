eb37bcf8a110ba0309050dfb1c731d3462311ac9
PerpetualPhoenix12 <38158382+PerpetualPhoenix12@users.noreply.github.com>
2018-10-12 00:40:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ import math
 
 xpban=[[""] * 2 for i in range(1)]
 
-sectList = ["Autarch Flipping", "Explosion","Blank","Thousand and One Petals","Debauchery Tea Party","Sort Post A Massege", "Rising Peak","NOT A GAE","Myriad ways to become a master of DAO"] #Name of Sect
-sectCall = ["ELON","Exp","Blank","PETALS","TeaParty","spam","Lord","gae","torii"] #call for .!sect <name>
-sectTags = ["「 ELON 」","Explosion","『　　』","《 PETALS 》","[TeaParty]","[spam]","Lord","【GAE】","〈ＴＯＲＩＩ〉"] #Tag required for xp   
+sectList = ["Autarch Flipping", "Explosion","Blank","Thousand and One Petals","Debauchery Tea Party","Sort Post A Massege", "Rising Peak","NOT A GAE","Pax Romana"] #Name of Sect
+sectCall = ["ELON","Exp","Blank","PETALS","TeaParty","spam","Lord","gae","spqr"] #call for .!sect <name>
+sectTags = ["「 ELON 」","Explosion","『　　』","《 PETALS 》","[TeaParty]","[spam]","Lord","【GAE】","「ＳＰＱＲ」"] #Tag required for xp   
 sectOwner = ["Perpetual Phoenix", "Megumin_Explosion", "Weiqing","Ziyun","ZaChan","VANSTORM","Modsan","WitheringLeaf","Myriad_"] #Owner of Sect
 sectDescription = ["Listen, ye mortal of the finite realms, for We are the Autarchs that were present at the beginning, and shall be present at the end! We are the Holy Inheritors of Elon, transcendent of all other races! We have seen generations trickle by, in the river of history, yet remain unfettered by time! Those who proclaim themselves as people of power are nothing to Us, for there is no apex past Us! A force that rivals the Heavens! A force that has become the Heavens! Who dares claim to be our equal‽ ",  #Elon
                     "EXPlOSION that's all\nFounded by megumin", #Explosion
