c3fd02ac2eb2d213adebc0f6132b9c7022e9cabb
PerpetualPhoenix12 <38158382+PerpetualPhoenix12@users.noreply.github.com>
2018-12-19 20:10:17 +0000

    Update roles.py

--- src/utils/roles.py
+++ src/utils/roles.py
@@ -36,8 +36,7 @@ class Roles:
 
 	@commands.command(pass_context=True)
 	async def roles(self, ctx):
-		allRoles = ["Literary Snobs", "Polygamist Reader", "Habitual Book Clubber", "Stockpiler", "Re-Reader", "Physical Book Loyalist", "Spoiler Lover", "Nonfiction Lover", "Fiction Fanatics", "Emotional Reader", "Book Juggler", "Die-Hard Reader",
-					"Weekend Warrior Reader", "Obsessive Bibliophile", "Tentative Reader", "Shy Reader", "Librocubicularist", "Grumpy Reader", "Book Eater", "Seasonal Reader", "Trend Reader", "Moody Reader", "Practical Reader", "Slow Reader"]
+		allRoles = usableRoles
 		embed = discord.Embed(
 			title=f"**There are {len(allRoles)} self-assignable roles.**", color=16711680)
 		pages = {}
